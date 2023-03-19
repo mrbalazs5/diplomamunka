b8c947ef6f55e4fcfcfc294480b7910a046aef49
mrbalazs5 <megbalaa@gmail.com>
2023-03-19 13:38:40 +0100

    modify dockerfiles according to Google Cloud 5

--- app/qg_app_frontend/vite.config.ts
+++ app/qg_app_frontend/vite.config.ts
@@ -1,21 +1,26 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
-    host: true,
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      port: 443,
+    },
     watch: {
-      usePolling: true
-    }
-  }
-})
+      usePolling: true,
+    },
+  },
+});
